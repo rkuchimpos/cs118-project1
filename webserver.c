f0312465b056bb40bde2da8042791315e51b3673
Jesse Catalan <Jesse@Jesses-MacBook-Pro.local>
2019-04-23 13:10:24 -0700

    Initialize the server

--- webserver.c
+++ webserver.c
@@ -1,6 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <netinet/in.h>
+
+#define PORTNO 33623
+#define BACKLOG 10
 
 int main() {
-	printf("Hello world\n");
-	return 0;	
+	int sockfd; /* listen on sock_fd */
+	int new_fd; /* new connection on new_fd */
+	struct sockaddr_in server_addr; /* my address */
+	struct sockaddr_in client_addr; /* connector addr */
+	int sin_size;
+
+ 	/* create socket */
+
+	if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) == -1) {
+		perror("Failed to create socket!\n");
+		exit(-1);
+	}
+
+	/* fill fields of sockaddr_in */
+
+	memset(&server_addr, 0, sizeof(server_addr));
+	server_addr.sin_family = PFINET;
+	server_addr.sin_port = htons(PORTNO);
+	my_addr.sin_addr.s_addr = htonl(INADDR_ANY); /* should not depend on htonl */
+
+	/* bind socket */
+
+	if (bind(sockfd, (struct sockaddr *) &server_addr, sizeof(struct sockaddr)) == -1) {
+		perror("Failed to bind socket!\n");
+		exit(-1);
+	}
+
+	/* listen for connections on the socket */
+
+	if (listen(sockfd, BACKLOG) == -1) {
+		perror("Failed to listen!\n")
+		exit(-1);
+	}
+
+	while(1) {
+		
+	}
+
 }
